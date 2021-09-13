534db4e5065ec4a22a1b7eaf6e8ec53073634b58
ufierro <ufierroruvalcaba@gmail.com>
2021-09-12 21:43:25 -0600

    Added regex to remove no alphanumeric characters from table names, working against multiple edge cases

--- cmd.go
+++ cmd.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"regexp"
 	"strings"
 
 	"github.com/codenotary/immudb/pkg/api/schema"
@@ -15,21 +16,23 @@ import (
 )
 
 var config struct {
-	hostname string
-	port     int
-	username string
-	password string
-	database string
-	filename string
+	hostname       string
+	port           int
+	username       string
+	password       string
+	database       string
+	filename       string
+	suiteTableName string
 }
 
 func initConfig() {
-	flag.StringVar(&config.hostname, "hostname", "", "Hostname or IP address for immudb")
+	flag.StringVar(&config.hostname, "hostname", "localhost", "Hostname or IP address for immudb")
 	flag.IntVar(&config.port, "port", 3322, "Port number of immudb server")
 	flag.StringVar(&config.username, "username", "immudb", "Username for authenticating to immudb")
 	flag.StringVar(&config.password, "password", "immudb", "Password for authenticating to immudb")
 	flag.StringVar(&config.database, "database", "defaultdb", "Name of the database to use")
 	flag.StringVar(&config.filename, "filename", "junit.xml", "The name of file, accepts comma separated values")
+	flag.StringVar(&config.suiteTableName, "summary_tbl_name", "junit_suite_summary", "The name of table used for test suite summary, creates new one if it doesn't exist already")
 	flag.Parse()
 }
 
@@ -77,33 +80,51 @@ func testSuiteToImmudb(parsed []junit.Suite) {
 	}
 	ctx = metadata.NewOutgoingContext(ctx, metadata.Pairs("authorization", udr.GetToken()))
 	for _, s := range parsed {
-		// The following relations should hold true.
-		//   Error == nil && (Status == Passed || Status == Skipped)
-		//   Error != nil && (Status == Failed || Status == Error)
-		log.Printf("Executing 'create table if not exists' for %s", s.Name)
-		_, err := client.SQLExec(ctx, fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (id INTEGER AUTO_INCREMENT, name VARCHAR NOT NULL, classname VARCHAR, duration BLOB, status BLOB, message VARCHAR, error BLOB, properties VARCHAR, systemout VARCHAR[256], systemerr VARCHAR[256], PRIMARY KEY id);", s.Name), nil)
+		log.Printf("Processing suite: %s", s.Name)
+		log.Printf("Executing: 'create table if not exists' for suite %s, characters not allowed in table names will be replaced by underscores", s.Name)
+		reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+		if err != nil {
+			log.Fatalf("Error removing stripping %s, consider renaming your test suite", s.Name)
+		}
+		processedString := reg.ReplaceAllString(s.Name, "")
+		formattedName := strings.Replace(processedString, " ", "_", -1)
+		_, err = client.SQLExec(ctx, fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (id INTEGER AUTO_INCREMENT, name VARCHAR NOT NULL, classname VARCHAR, duration BLOB, status BLOB, message VARCHAR, error BLOB, properties BLOB, systemout VARCHAR[256], systemerr VARCHAR[256], PRIMARY KEY id);", formattedName), nil)
 		if err != nil {
 			log.Fatalf("Error creating table %s", s.Name)
 		}
-		_, err = client.SQLExec(ctx, fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s_runs (id INTEGER AUTO_INCREMENT, name VARCHAR, package VARCHAR, properties BLOB, tests BLOB, suites BLOB, systemout VARCHAR[256], systemerr VARCHAR[256], totals BLOB, PRIMARY KEY id)", s.Name), nil)
+		_, err = client.SQLExec(ctx, fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (id INTEGER AUTO_INCREMENT, name VARCHAR, package BLOB, properties BLOB, tests BLOB, suites BLOB, systemout VARCHAR[256], systemerr VARCHAR[256], totals BLOB, PRIMARY KEY id)", config.suiteTableName), nil)
 		if err != nil {
 			log.Println(err.Error())
 			log.Fatalf("Error creating table %s", s.Name)
 		}
-		t, err := json.Marshal(s.Totals)
 		if err != nil {
 			log.Fatal("Error marshaling parsed response from junit file")
 		}
-		_, err = client.SQLExec(ctx, fmt.Sprintf("INSERT INTO %s_runs (name, package,totals) VALUES (@name, @package, @totals)", s.Name), map[string]interface{}{"name": s.Name, "package": s.Package, "totals": t})
+		p, err := json.Marshal(s.Package)
+		props, err := json.Marshal(s.Properties)
+		t, err := json.Marshal(s.Tests)
+		suites, err := json.Marshal(s.Suites)
+		totals, err := json.Marshal(s.Totals)
 		if err != nil {
-			log.Fatal("Error inserting into database")
+			log.Fatal("Error marshalling parser response for test suite summary")
+		}
+		_, err = client.SQLExec(
+			ctx, fmt.Sprintf("INSERT INTO %s (name, package, properties, tests, suites, systemout, systemerr, totals) VALUES (@name, @package, @properties, @tests, @suites, @systemout, @systemerr, @totals)",
+				config.suiteTableName), map[string]interface{}{"name": s.Name, "package": p, "properties": props, "tests": t, "suites": suites, "systemout": s.SystemOut, "systemerr": s.SystemErr, "totals": totals})
+		if err != nil {
+			log.Println(err.Error())
+			log.Fatal("Error inserting suite results into database")
 		}
 		for _, t := range s.Tests {
-			log.Println(t.Name)
-			duration, _ := json.Marshal(t.Duration)
-			tError, _ := json.Marshal(t.Error)
-			status, _ := json.Marshal(t.Status)
-			_, err = client.SQLExec(ctx, fmt.Sprintf("INSERT INTO %s (name, classname, duration, status, message, error) VALUES (@name, @classname, @duration, @status, @message, @error)", s.Name), map[string]interface{}{"name": t.Name, "classname": t.Classname, "duration": duration, "status": status, "message": t.Message, "error": tError})
+			d, err := json.Marshal(t.Duration)
+			p, err := json.Marshal(t.Properties)
+			s, err := json.Marshal(t.Status)
+			e, err := json.Marshal(t.Error)
+			if err != nil {
+				log.Fatalf("Error marshalling parser response for %s", t.Name)
+			}
+			//log.Println(fmt.Sprintf("Processing test case: %s", t.Name))
+			_, err = client.SQLExec(ctx, fmt.Sprintf("INSERT INTO %s (name, classname, duration, status, message, error, properties, systemout, systemerr) VALUES (@name, @classname, @duration, @status, @message, @error, @properties, @systemout, @systemerr)", formattedName), map[string]interface{}{"name": t.Name, "classname": t.Classname, "duration": d, "status": s, "message": t.Message, "error": e, "properties": p, "systemout": t.SystemOut, "systemerr": t.SystemErr})
 			if err != nil {
 				log.Fatalf("Error inserting test results: %s", err.Error())
 			}
@@ -119,5 +140,6 @@ func main() {
 		log.Fatalf("Failed to parse file, error: %s", config.filename)
 	}
 	testSuiteToImmudb(response)
+	log.Println("Finished exporting junit results to immudb")
 
 }
